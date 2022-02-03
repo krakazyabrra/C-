c8c3641db95cfa38bbc8d89016cba45234ca0596
krakazyabrra <98947126+krakazyabrra@users.noreply.github.com>
2022-02-03 22:53:24 +0300

    program_1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program1
+{
+    public class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Imput your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi, " + name);
+            Console.WriteLine("Press <Enter> for exit");
+            Console.ReadLine();
+        }    
+    }
+
+}
